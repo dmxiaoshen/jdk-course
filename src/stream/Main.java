234dba481fae79d772b34d13f537b3dfb64d4a8c
hushengen <hsg@dmxiaoshen.com>
2018-07-13 18:29:06 +0800

    lambda

---
+++ src/stream/Main.java
@@ -0,0 +1,18 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by hzhsg on 2018/7/13.
+ */
+public class Main {
+
+    private  static List<Student> studentList = Arrays.asList(
+            new Student("小红","女",15,98.5),
+            new Student("小明","男",16,90.5),
+            new Student("冬冬","女",16,88.5),
+            new Student("丁丁","男",14,99.5),
+            new Student("Lucy","女",17,60)
+    );
+}
